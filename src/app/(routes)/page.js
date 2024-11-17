a7ff40297ad9e07d7c57ed8f8bd17aecf8188809
VivekDudi-Github <vivekdudi18@gmail.com>
2024-11-17 17:49:44 +0530

    initial commit

---
+++ src/app/(routes)/page.js
@@ -0,0 +1,36 @@
+import { auth, signIn, signOut } from "../../auth";
+
+export default async function Home() {
+  const session = await auth();
+if(!session){
+  return (
+    <div className="">
+      <form
+        action={async () => {
+          "use server"
+          await signIn("google")
+        }}
+      >
+        <button 
+        className="border-2 bg-orangish rounded-xl p-4" 
+        type="submit"
+        >
+          Signin with Google
+          </button>
+      
+      </form>
+      
+    </div>
+    );
+}
+  return (
+  <div className="">
+      <button onClick= {async () => {
+        "use server"
+        await signOut("google")} }>
+      LOgOUt
+    </button>
+    
+  </div>
+  );
+}
